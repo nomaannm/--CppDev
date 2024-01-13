e4057d97043101a0b333e28b9932298796c2b7f4
Noman Mumtaz <nomanm2324@gmail.com>
2024-01-13 20:25:02 +0530

    Create CharacterLiterals.cpp

---
+++ CharacterLiterals.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+
+int main() {
+	std::cout << "\n New Line Character \n";
+	std::cout << "\t Horizontal Tab \n";
+	std::cout << "\v\v Vertical Tab \n";
+	std::cout << "Question mark \? \n";
+	std::cout << "BackSpace\b";
+	std::cout << "\nAlert\a";
+	std::cout << "\n Carriage Return \r Return Carriage \n";
+	std::cout << "Hello\fWorld\n";
+	std::cout << "\x6F \n";
+
+}
