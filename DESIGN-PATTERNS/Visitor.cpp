aa2e0e103370a97d06b19d1068bf5c06ec8d3e17
nomaannm <nomanm2324@gmail.com>
2024-07-29 18:11:27 +0530

    Fixed a bug

--- DESIGN-PATTERNS/Visitor.cpp
+++ DESIGN-PATTERNS/Visitor.cpp
@@ -1,5 +1,43 @@
-//
-// Created by noman on 29-07-2024.
-//
-
 #include "Visitor.h"
+
+void Hardwaredev::visit(const Console *playStation) const {
+        std::cout << "Fixed the playstation!\n";
+}
+
+void Hardwaredev::visit(const Laptop *macBook) const {
+    std::cout << "Fixed macbook!\n";
+}
+
+void Softwaredev::visit(const Console *playStation) const {
+    std::cout << "Installed the latest software in the PS.\n";
+}
+
+void Softwaredev::visit(const Laptop *macBook) const {
+    std::cout << "Installed the latest software in this MacBook.\n";
+}
+
+void Console::accept(Dev *visitor) const {
+    visitor->visit(this);
+}
+
+void Laptop::accept(Dev *visitor) const {
+    visitor->visit(this);
+}
+
+
+int main() {
+    Console Playstation;
+    Laptop Mac;
+
+    Hardwaredev hardwaredev;
+    Softwaredev softwaredev;
+
+    Playstation.accept(&hardwaredev);
+    Playstation.accept(&softwaredev);
+
+    Mac.accept(&hardwaredev);
+    Mac.accept(&softwaredev);
+    return 0;
+}
+
+
--- DESIGN-PATTERNS/Visitor.h
+++ DESIGN-PATTERNS/Visitor.h
@@ -1,5 +1,52 @@
 #ifndef VISITOR_H
 #define VISITOR_H
+#pragma once
+#include <iostream>
+
+/*
+ * The Visitor design pattern is a behavioral pattern used to separate an algorithm from the object structure on which it operates.
+ * This pattern allows you to add new operations to a collection of objects without modifying their classes.
+ */
+
+class Dev;
+
+//Visitable
+class Product {
+public:
+    virtual void accept(Dev* visitor) const = 0;
+};
+
+class Console : public Product {
+public:
+    void accept(Dev *visitor) const override;
+};
+
+class Laptop : public Product {
+public:
+    void accept(Dev *visitor) const override;
+};
+
+//Visitor
+class Dev {
+public:
+       virtual ~Dev() = default;
+       virtual void visit(const Console* playStation) const = 0;
+       virtual void visit(const Laptop* macBook) const = 0;
+};
+
+class Hardwaredev : public Dev {
+public:
+       void visit(const Console* playStation) const override;
+       void visit(const Laptop* macBook) const override;
+};
+
+class Softwaredev : public Dev {
+public:
+        void visit(const Console* playStattion) const override;
+        void visit(const Laptop* macBook) const override;
+};
+
+
 
 
 #endif //VISITOR_H
